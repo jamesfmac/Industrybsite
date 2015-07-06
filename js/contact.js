a8731ef61f748c2ff455756420270b077ad4af86
James <jfmcmanamey@gmail.com>
2015-07-07 00:11:44 +1000

    Remove recaptcha on small screens

--- js/contact.js
+++ js/contact.js
@@ -1,5 +1,28 @@
-var enableBtn = function(){
-document.getElementById("btnSubmit").disabled = false;
+var enableBtn = function () {
+    document.getElementById("btnSubmit").disabled = false;
 }
 
+var mq = window.matchMedia('all and (max-width: 700px)');
+if (mq.matches) {
+    // the width of browser is more then 700px
 
+     document.getElementById("divCaptcha").style.display = 'none';
+    document.getElementById("btnSubmit").disabled = false;
+} else {
+    // the width of browser is less then 700px
+    document.getElementById("divCaptcha").style.display = 'block';
+    document.getElementById("btnSubmit").disabled = true;
+}
+
+mq.addListener(function (changed) {
+    if (changed.matches) {
+        // the width of browser is more then 700px
+
+         document.getElementById("divCaptcha").style.display = 'none';
+        document.getElementById("btnSubmit").disabled = false;
+    } else {
+        // the width of browser is less then 700px
+           document.getElementById("divCaptcha").style.display = 'block';
+        document.getElementById("btnSubmit").disabled = true;
+    }
+});
